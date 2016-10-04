5b7ec921edd69ee6f2dd4164434181d3713b2ef6
gearazk <stricker.reno@gmail.com>
2016-10-05 00:31:59 +0700

    fix search

--- app/models/artical.rb
+++ app/models/artical.rb
@@ -1,6 +1,7 @@
 class Artical < ActiveRecord::Base
 	def self.search(search)
-	  where("title ILIKE ?", "%#{search}%") 
-	  where("body ILIKE ?", "%#{search}%")
+		where('(title ILIKE ? or body ILIKE ?)', "%#{search}%" , "%#{search}%")
+	  # where("title ILIKE ?", "%#{search}%")|| 
+	  # where("body ILIKE ?", "%#{search}%")
 	end
 end
